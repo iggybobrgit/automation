7fd1c3b0f963ccd22113ad55ef2d7ab986566b1c
Igor Bobrov <igorbobrov@coherentsolutions.com>
2022-02-28 18:04:01 +0300

    Homework5

---
+++ Homework5/Library.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework5
+{
+    class Library
+    {
+         string bookName;
+         int bookPages;
+         string bookID;
+
+
+        public Library (string bookname, int bookpages, string bookid)
+        {
+            bookName = bookname;
+            bookPages = bookpages;
+            bookID = bookid;
+        }
+
+        public string GetBookName()
+        {
+            return bookName;
+        }
+
+        public int  GetBookPages()
+        {
+            return bookPages;
+        }
+
+        public string GetBookID()
+        {
+            return bookID;
+        }
+    }
+}
