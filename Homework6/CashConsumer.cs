9a5e5ce7038de931c1cc628f295f4bca25c6e35e
Igor Bobrov <igorbobrov@coherentsolutions.com>
2022-03-14 18:04:21 +0300

    Homework6

---
+++ Homework6/CashConsumer.cs
@@ -0,0 +1,10 @@
+﻿namespace Homework6
+{
+    class CashConsumer: Consumer
+    {
+        public override string MakePayment()
+        {
+            return System.String.Format("Consumer paid by Cash");
+        }
+    }
+}
