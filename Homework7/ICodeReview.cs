cf57fa1ec35e44170c162da42b7816e149db7197
Igor Bobrov <igorbobrov@coherentsolutions.com>
2022-03-15 21:13:20 +0300

    Homework7

---
+++ Homework7/ICodeReview.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework7
+{
+    interface ICodeReview
+    {
+        void CodeReview(int ID, string FirstName, string LastName);
+    }
+}
