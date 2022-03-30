6813de365a2ca9cfb10f0cfd71227fdc2c5b2636
Igor Bobrov <igorbobrov@coherentsolutions.com>
2022-03-30 17:25:25 +0300

    Homework10

---
+++ Homework10/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework10
+{
+    class Book
+    {
+        public Book (string bookname, DateTime printdate, int bookid, List<Author> authors)
+        {
+            BookName = bookname;
+            PrintDate = printdate;
+            BookID = bookid;
+            Authors = authors;
+        }
+        public string BookName { get; set; }
+        public DateTime PrintDate { get; set; }
+        public int BookID { get; set; }
+        public List <Author> Authors  { get; set; }
+    }
+}
---
+++ Homework10/Catalog.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Homework10
+{
+    class Catalog
+    {
+        public List<Book> AvailableBooks { get; set; }
+        
+        public List<Book> GetBookNames(List<Book> books)
+        {
+            Console.WriteLine("List of books ordered by book name:");
+            var names = books.OrderBy(x => x.BookName).ToList();
+            return names;
+        }
+
+        public List<Author> GetAuthorsByBirthday(List<Author> authors)
+        {
+            Console.WriteLine("List of all authors ordered by DOB:");
+            var names = authors.OrderBy(x => x.DOB).ToList();
+            return names;
+        }
+
+        public List<Book> GetCobenLeeBooks(List<Book> books)
+        {
+            Console.WriteLine("List of books wrote by Coben Lee after 01/01/2001");
+            var names = books.Where(x => x.Authors.Any(a =>a.FirstName == "Lee" && a.LastName == "Coben")).Where(x => x.PrintDate > new DateTime(2001, 01, 01)).ToList();
+            return names;
+        }
+
+        public List<Author> GetListOfBookWAvailableAuthor(List<Book> books)
+        {
+            Console.WriteLine("List of books wrote by authors from catalog");
+            var names = books.SelectMany(x => x.Authors).Distinct().ToList();
+            return names;
+        }
+    }
+}
