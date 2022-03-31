34e13fef73282730ecbad1df73880eb2a41a4b45
Igor Bobrov <igorbobrov@coherentsolutions.com>
2022-03-31 15:26:37 +0300

    Homework9

---
+++ Homework9/PMEployee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework9
+{
+    class PMEployee : Employee, IAssigneTask
+    {
+        public void AssigneTask(int ID, string FirstName, string LastName)
+        {
+            Console.WriteLine("{0} {1} {2}", ID, FirstName, LastName) ;
+        }
+    }
+}
+
