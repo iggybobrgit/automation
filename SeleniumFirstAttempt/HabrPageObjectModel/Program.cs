97ba7a1c739bc3eaa62d6946b47703560b5aa000
Igor Bobrov <igorbobrov@coherentsolutions.com>
2022-04-19 10:24:04 +0300

    Habr.com autotests

---
+++ SeleniumFirstAttempt/HabrPageObjectModel/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HabrPageObjectModel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ SeleniumFirstAttempt/HabrPageObjectModel/Waiter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+namespace HabrPageObjectModel
+{
+    public static class Waiter
+    {
+        public static void WaitElement(IWebDriver webDriver, By locator, int seconds = 5)
+        {
+            new WebDriverWait(webDriver, TimeSpan.FromSeconds(seconds)).Until(ExpectedConditions.ElementIsVisible(locator));
+            new WebDriverWait(webDriver, TimeSpan.FromSeconds(seconds)).Until(ExpectedConditions.ElementToBeClickable(locator));
+        }
+    }
+}
