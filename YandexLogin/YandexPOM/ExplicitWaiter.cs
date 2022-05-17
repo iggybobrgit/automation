9580f4af2662be2b2bc64ede30111b8978b23cb6
Igor Bobrov <IgorBobrov@coherentsolutions.com>
2022-05-17 22:17:05 +0300

    Yandex login test

---
+++ YandexLogin/YandexPOM/ExplicitWaiter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+
+namespace YandexPOM
+{
+    public static class ExplicitWaiter
+    {
+        public static void WaitElement(IWebDriver webDriver, By locator, int seconds = 10)
+        {
+            try
+            {
+                new WebDriverWait(webDriver, TimeSpan.FromSeconds(seconds)).Until(ExpectedConditions.ElementIsVisible(locator));
+                new WebDriverWait(webDriver, TimeSpan.FromSeconds(seconds)).Until(ExpectedConditions.ElementToBeClickable(locator));
+            }
+            catch(WebDriverTimeoutException ex)
+            {
+                throw new NotFoundException($"Cannot find specific location: {locator}", ex);
+            }
+
+        }
+    }
+}
---
+++ YandexLogin/YandexPOM/YandexMailPage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace YandexPOM
+{
+    public class YandexMailPage
+    {
+        private IWebDriver _webDriver;
+
+        public YandexMailPage(IWebDriver webdriver)
+        {
+            _webDriver = webdriver;
+        }
+    }
+}
