295fa8d9f9697360c9fe83f6148d21b254878cb9
iggybobrgit <iggybobr@gmail.com>
2022-05-17 23:55:05 +0300

    Create test with multiselect (URL - https://demo.seleniumeasy.com/basic-select-dropdown-demo.html). Select 3 random options in “Multi Select List Demo” section and verify that they are selected (for this case don’t work with UI verification)

---
+++ Task50_5/Task50_Subtask5/SelectCheck.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task50_Subtask5
+{
+    public class VerifyMultiSelection
+    {
+        private IWebDriver _driver;
+        private List<string> _optionsToSelect;
+        private List<string> _selectedOptions;
+
+        public VerifyMultiSelection(IWebDriver driver, List<string> optionsToSelect, List<string> selectedOptions)
+        {
+            _driver = driver;
+            _optionsToSelect = optionsToSelect;
+            _selectedOptions = selectedOptions;
+        }
+
+        public bool IsEqual()
+        {
+            return _optionsToSelect.SequenceEqual(_selectedOptions);
+        }
+    }
+}
