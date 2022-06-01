f399fd7e77226a5cf8521c5c4467e26e181a9dff
Igor Bobrov <igorbobrov@coherentsolutions.com>
2022-06-01 14:54:34 +0300

    task80

---
+++ Task80/Task80/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task80
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
