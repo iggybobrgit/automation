c741f7d93656bdcf38325c4d240db72ceada61fb
Igor Bobrov <igorbobrov@coherentsolutions.com>
2022-08-05 14:21:06 +0300

    saucelab

---
+++ SauceLab/Task70/BrowserContext.cs
@@ -0,0 +1,49 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task80
+{
+    public class BrowserContext
+    {
+        protected static IWebDriver driver;
+        private static string _sauceUserName;
+        private static string _sauceAccessKey;
+        private static Dictionary<string, object> _sauceOptions;
+        public static void BrowserSetup()
+        {
+            {
+                    _sauceUserName = Environment.GetEnvironmentVariable("igorbobrov");
+                    _sauceAccessKey = Environment.GetEnvironmentVariable("ed2c19f2-9c04-413a-8de6-74b5b3aa5e40");
+                    _sauceOptions = new Dictionary<string, object>
+                    {
+                        ["username"] = _sauceUserName,
+                        ["accessKey"] = _sauceAccessKey,
+                        ["name"] = "TestMozillaon8.1"
+                    };
+
+                var browserOptions = new FirefoxOptions
+                {
+                    BrowserVersion = "latest",
+                    PlatformName = "Windows 8.1"
+                };
+
+                browserOptions.AddAdditionalOption("sauce:options", _sauceOptions);
+                    driver = new RemoteWebDriver(new Uri("https://igorbobrov:ed2c19f2-9c04-413a-8de6-74b5b3aa5e40@ondemand.saucelabs.com/wd/hub"), browserOptions.ToCapabilities(),
+                    TimeSpan.FromSeconds(30));
+                    driver.Navigate().GoToUrl("https://yandex.by");
+                    driver.Manage().Timeouts().ImplicitWait = System.TimeSpan.FromSeconds(7);          
+            }
+        }
+
+        public static void BrowserExit()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ SauceLab/Test/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+using Task80;
+using SeleniumExtras.WaitHelpers;
+using Allure.Commons;
+using NUnit.Allure.Attributes;
+using NUnit.Allure.Core;
+using System.Collections.Generic;
+using System;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Remote;
+
+namespace YandexLoginTests
+{
+    public class Tests : BrowserContext
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+            BrowserContext.BrowserSetup();            
+        }
+       
+        [TestCase("iggybobr", "Panasonik99659965")]        
+        public void YandexMailLogin(string login, string password)
+        {           
+            var mainPage = new YandexMainPage(driver);
+            var signinMenu = mainPage.OpenSignInMenu();
+            signinMenu.EnterLogin(login);
+            signinMenu.EnterPassword(password);
+            Assert.AreEqual(login, mainPage.GetLoggedInSign());
+        }
+       
+        [TestCase("iggybobr", "Panasonik99659965")]
+        public void YandexMailLogOut(string login, string password)
+        {
+            var mainPage = new YandexMainPage(driver);
+            var signinMenu = mainPage.OpenSignInMenu();
+            signinMenu.EnterLogin(login);
+            signinMenu.EnterPassword(password);
+            mainPage.Logout();
+            Assert.AreEqual("Войти", mainPage.GetLoggedOutSign());      
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            BrowserContext.BrowserExit();
+        }
+    }
+}
